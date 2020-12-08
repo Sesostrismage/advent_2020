db8e7d22fa9e69657eefb376b3b9ec86e745d2a3
Nick Weppenaar <nick.weppenaar@maersk.com>
2020-12-08 06:27:22 +0100

    Day 8 part 2.

--- 2020/day_08/day_08.py
+++ 2020/day_08/day_08.py
@@ -8,8 +8,6 @@ df = pd.read_csv(
     header=None
 )
 
-print(df.head())
-
 def run_code(code_set):
     idx = 0
     existing_idx_list = []
@@ -43,5 +41,21 @@ def run_code(code_set):
 
 # Part 1.
 a, s = run_code(df)
+
+print(f"Acc value: {a}, status: {s}")
+
+# Part 2.
+for i in range(len(df)):
+    if df.iloc[i, 0] == 'jmp':
+        new_code = df.copy()
+        new_code.iloc[i, 0] = 'nop'
+        a, s = run_code(new_code)
+    elif df.iloc[i, 0] == 'nop':
+        new_code = df.copy()
+        new_code.iloc[i, 0] = 'jmp'
+        a, s = run_code(new_code)
+
+    if s == 'terminated':
+        break
 
 print(f"Acc value: {a}, status: {s}")
