800699a8695679067e32012ba1da805b2179018f
Nick Weppenaar <nick.weppenaar@maersk.com>
2020-12-08 11:02:32 +0100

    2019 day 2 part 2.

--- year_2019/day_02/day_02.py
+++ year_2019/day_02/day_02.py
@@ -10,8 +10,26 @@ f.close()
 
 code_list = [int(item) for item in txt.split(',')]
 
+# Part 1.
 ic = IntCode(code_list, 12, 2)
 output = ic.run_code()
 
-
 print(f"Value at position 0: {output}")
+
+# Part 2.
+desired_output = 19690720
+
+for n in range(100):
+    for m in range(100):
+        ic = IntCode(code_list, n, m)
+        output = ic.run_code()
+
+        if output == desired_output:
+            print('Match!')
+            print(f"Input 1: {n}, input 2: {m}")
+            break
+
+    if output == desired_output:
+        break
+
+print(f"Result: {100 * n + m}")
