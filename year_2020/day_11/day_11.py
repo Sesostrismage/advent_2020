98afa9bab5b3a91a768d2b52db92158e572da5a8
Nick Weppenaar <nick.weppenaar@maersk.com>
2020-12-11 06:53:49 +0100

    Initial code for day 11 part 1, doesn't work yet.

---
+++ year_2020/day_11/day_11.py
@@ -0,0 +1,44 @@
+import numpy as np
+import os
+import pandas as pd
+
+curr_dir = os.path.dirname(os.path.abspath(__file__))
+
+f = open(os.path.join(curr_dir, "data.txt"))
+txt = f.readlines()
+f.close()
+
+txt_cleaned = [item.strip() for item in txt]
+txt_split = [list(item) for item in txt_cleaned]
+df = pd.DataFrame.from_records(txt_split).replace({'L': 0, '.': np.nan})
+
+# print(df)
+
+df_count = df.copy()
+df_prev = df.copy()
+c = 1
+
+while True:
+    print(f"Iteration {c}")
+
+    for i, row in df.iterrows():
+        for j, item in row.iteritems():
+            local_sum = df.iloc[i-1:i+2, j-1:j+2].sum().sum() - df.iloc[i, j]
+            df_count.iloc[i, j] = local_sum
+
+    for i, row in df_count.iterrows():
+        for j, item in row.iteritems():
+            if df.iloc[i, j] == 0 and df_count.iloc[i, j] == 0:
+                df.iloc[i, j] = 1
+            elif df.iloc[i, j] == 1 and df_count.iloc[i, j] >= 4:
+                df.iloc[i, j] = 0
+
+    print(f"{df.sum().sum()} occupied seats.")
+
+    if df.equals(df_prev):
+        break
+    else:
+        df_prev = df.copy()
+        c += 1
+
+print(f"{df.sum().sum()} occupied seats.")
