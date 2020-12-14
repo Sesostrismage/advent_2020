9c5be4d418d1e143e9a95dd3bff8677b46dc3ed4
Nick Weppenaar <nick.weppenaar@maersk.com>
2020-12-14 14:31:17 +0100

    Day 14 part 1.

---
+++ year_2020/day_14/day_14_part_1.py
@@ -0,0 +1,35 @@
+import os
+
+curr_dir = os.path.dirname(os.path.abspath(__file__))
+
+f = open(os.path.join(curr_dir, "data.txt"))
+# f = open(os.path.join(curr_dir, "test.txt"))
+txt = f.readlines()
+f.close()
+
+txt_cleaned = [item.strip() for item in txt]
+
+mask = 'X'*36
+mem = {}
+
+for item in txt_cleaned:
+    if item.startswith('mask'):
+        mask = item[7:]
+    else:
+        split = item.split('=')
+        address = int(split[0][4:-2])
+        val = int(split[1])
+        val_bin = bin(val)[2:].zfill(36)
+
+        for idx, m in enumerate(mask):
+            if m != 'X':
+                val_bin = val_bin[:idx] + m + val_bin[idx + 1:]
+
+        mem[address] = val_bin
+
+total = 0
+
+for address in mem:
+    total += int(mem[address], 2)
+
+print(total)
