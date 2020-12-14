3edd6631393401cfc0ac72f0a74de6633993c52a
Nick Weppenaar <nick.weppenaar@maersk.com>
2020-12-14 07:58:24 +0100

    Day 11 part 1 now works. Added test file for part 1.

--- year_2020/day_11/day_11.py
+++ year_2020/day_11/day_11.py
@@ -5,6 +5,7 @@ import pandas as pd
 curr_dir = os.path.dirname(os.path.abspath(__file__))
 
 f = open(os.path.join(curr_dir, "data.txt"))
+# f = open(os.path.join(curr_dir, "test_part_1.txt"))
 txt = f.readlines()
 f.close()
 
@@ -21,13 +22,18 @@ c = 1
 print(f"Initial number of seats: {df.notnull().sum().sum()}")
 
 while True:
-    print(f"Iteration {c}")
+    # df.to_excel(f"C:/Kode/advent_of_code/year_2020/day_11/df_{c}.xlsx")
+    # df_count.to_excel(f"C:/Kode/advent_of_code/year_2020/day_11/df_count_{c}.xlsx")
+    max_row = df.shape[0]
+    max_col = df.shape[1]
+
+    print(f"\nIteration {c}")
 
     for i, row in df.iterrows():
         for j, item in row.iteritems():
             local_sum = df.iloc[
-                max(0, i-1):min(i+2, df.index[-1]),
-                max(0, j-1):min(j+2, df.columns[-1])
+                max(0, i-1):min(i+2, max_row),
+                max(0, j-1):min(j+2, max_col)
             ].sum().sum() - df.iloc[i, j]
             df_count.iloc[i, j] = local_sum
 
