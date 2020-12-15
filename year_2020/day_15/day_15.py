6421f7fbdd453c70797b28b49fd99aa6153d92ad
Nick Weppenaar <nick.weppenaar@maersk.com>
2020-12-15 11:41:33 +0100

    Code formatting.

--- year_2020/day_15/day_15.py
+++ year_2020/day_15/day_15.py
@@ -14,44 +14,29 @@ def memory_game(m, l, s, stop):
     return l
 
 # Test.
-mem = {}
-mem[0] = 1
-mem[3] = 2
+mem = {0: 1, 3: 2}
 last = 6
 last = memory_game(mem, last, 4, 11)
 print(f"Test 1 result: {last}")
 
 # Test.
-mem = {}
-mem[0] = 1
-mem[3] = 2
+mem = {0: 1, 3: 2}
 last = 6
 last = memory_game(mem, last, 4, 2021)
 print(f"Test 2 results: {last}")
 
 # Part 1.
-mem = {}
-mem[2] = 1
-mem[15] = 2
-mem[0] = 3
-mem[9] = 4
-mem[1] = 5
+mem = {2: 1, 15: 2, 0: 3, 9: 4, 1: 5}
 last = 20
 last = memory_game(mem, last, 7, 2021)
 print(f"Part 1 results: {last}")
 
 t_start = datetime.datetime.now()
 # Part 2.
-mem = {}
-mem[2] = 1
-mem[15] = 2
-mem[0] = 3
-mem[9] = 4
-mem[1] = 5
+mem = {2: 1, 15: 2, 0: 3, 9: 4, 1: 5}
 last = 20
 last = memory_game(mem, last, 7, 30000001)
 print(f"Part 2 results: {last}")
 
 t_end = datetime.datetime.now()
-
 print(f"Part 2 time: {t_end - t_start}")
