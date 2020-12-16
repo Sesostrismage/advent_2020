8e1a0ede71e7564ab43ccde1415a5a12c7d792d4
Nick Weppenaar <weppenaar@gmail.com>
2020-12-16 07:38:04 +0100

    Part 2 solved.

--- year_2020/day_16/day_16.py
+++ year_2020/day_16/day_16.py
@@ -1,16 +1,15 @@
 import os
+import pandas as pd
 
 curr_dir = os.path.dirname(os.path.abspath(__file__))
 
-# f = open(os.path.join(curr_dir, "data.txt"))
+f = open(os.path.join(curr_dir, "data.txt"))
 # f = open(os.path.join(curr_dir, "test_part_1.txt"))
-f = open(os.path.join(curr_dir, "test_part_2.txt"))
+# f = open(os.path.join(curr_dir, "test_part_2.txt"))
 txt = f.readlines()
 f.close()
 
 txt_cleaned = [line.strip() for line in txt if line != '\n']
-
-# print(txt_cleaned)
 
 def extract_numbers(l, vr, r):
     parts = l.split(':')[1].split()
@@ -39,17 +38,19 @@ def check_ticket(l, e, vr):
 valid_range = []
 valid_ticket_list = []
 error = 0
-rules = {}
+rules_dict = {}
 
 state = 'rules'
 
 for line in txt_cleaned:
     if state == 'rules' and line != 'your ticket:':
-        valid_range, rules = extract_numbers(line, valid_range, rules)
+        valid_range, rules = extract_numbers(line, valid_range, rules_dict)
     elif line == 'your ticket:':
         state = 'my_ticket'
     elif line == 'nearby tickets:':
         state = 'nearby_tickets'
+    elif state == 'my_ticket':
+        my_ticket = [int(field) for field in line.split(',')]
     elif state == 'nearby_tickets':
         error, valid_ticket, valid_range = check_ticket(line, error, valid_range)
 
@@ -60,6 +61,38 @@ valid_range = sorted(list(set(valid_range)))
 print(f"Ticket scanning error rate: {error}")
 
 # Part 2.
-print(valid_ticket_list)
+ticket_df = pd.DataFrame.from_records(valid_ticket_list)
 
-# print(rules)
+possible_fields_dict = {col:[] for col in ticket_df.columns}
+
+for field in possible_fields_dict:
+    for rule in rules_dict:
+        if ticket_df[field].isin(rules_dict[rule]).all():
+            possible_fields_dict[field].append(rule)
+
+guessed_fields_dict = {}
+
+while True:
+    if len(possible_fields_dict) == 0:
+        break
+
+    for col in possible_fields_dict:
+        if len(possible_fields_dict[col]) == 1:
+            field = possible_fields_dict[col][0]
+            column = col
+            break
+
+    possible_fields_dict.pop(column)
+    guessed_fields_dict[column] = field
+
+    for column in possible_fields_dict:
+        if field in possible_fields_dict[column]:
+            possible_fields_dict[column].pop(possible_fields_dict[column].index(field))
+
+multiplier = 1
+
+for idx, number in enumerate(my_ticket):
+    if guessed_fields_dict[idx][:9] == 'departure':
+        multiplier = multiplier * number
+
+print(f"Part 2 answer: {multiplier}")
