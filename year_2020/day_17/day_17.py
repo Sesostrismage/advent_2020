a9772e99028f6e7ed3ecb278e8d613999dc82b2e
Nick Weppenaar <weppenaar@gmail.com>
2020-12-17 07:15:48 +0100

    Day 1 generalized.

--- year_2020/day_17/day_17.py
+++ year_2020/day_17/day_17.py
@@ -13,29 +13,24 @@ f.close()
 txt_cleaned = [item.strip() for item in txt]
 txt_split = [list(item) for item in txt_cleaned]
 df = pd.DataFrame.from_records(txt_split).replace({'#': 1, '.': 0})
+a = df.values
 
 def iterate(d, k, n):
-    for i in range(n):
+    for _ in range(n):
         c = convolve(d, k, mode='constant')
         inactivate = ((d == 1) & (c < 2)) | ((d == 1) & (c > 3))
         activate = ((d == 0) & (c == 3))
         d[inactivate] = 0
         d[activate] = 1
 
-    return np.sum(np.sum(np.sum(d)))
+    return d
 
-a = df.values
-a = a.reshape(a.shape + (1,))
-d = np.zeros([a.shape[0]+12, a.shape[1]+12, 13])
-d[6:-6, 6:-6, 6:-6] = a
-k = np.ones([3,3,3])
-k[1,1,1] = 0
 
-for n in range(6):
-    c = convolve(d, k, mode='constant')
-    inactivate = ((d == 1) & (c < 2)) | ((d == 1) & (c > 3))
-    activate = ((d == 0) & (c == 3))
-    d[inactivate] = 0
-    d[activate] = 1
-
-print(f"Number active: {np.sum(np.sum(np.sum(d)))}")
+# Part 1.
+a_part_1 = a.reshape(a.shape + (1,))
+dimension = np.zeros([a_part_1.shape[0]+12, a_part_1.shape[1]+12, 13])
+dimension[6:-6, 6:-6, 6:-6] = a_part_1
+kernel = np.ones([3,3,3])
+kernel[1,1,1] = 0
+dimension = iterate(dimension, kernel, 6)
+print(f"Number active, part 1: {np.sum(np.sum(np.sum(dimension)))}")
