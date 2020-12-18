d7122224751005b8b25c8fb91fc82079f668f95a
Nick Weppenaar <nick.weppenaar@maersk.com>
2020-12-18 07:21:23 +0100

    Da7 18 part 2.

--- year_2020/day_18/day_18.py
+++ year_2020/day_18/day_18.py
@@ -1,23 +1,64 @@
 import os
 
-def evaluate(expr):
+def evaluate_basic(expr):
     number = None
     operator = None
     expr_list = expr.split()
 
-    for item in expr_list:
-        if item in ['+', '*']:
-            operator = item
+    for component in expr_list:
+        if component in ['+', '*']:
+            operator = component
         elif operator is None:
-            number = int(item)
+            number = int(component)
         elif operator == '+':
-            number += int(item)
+            number += int(component)
             operator = None
         elif operator == '*':
-            number *= int(item)
+            number *= int(component)
             operator = None
 
     return number
+
+def evaluate_advanced(expr):
+    expr_list = expr.split()
+
+    while True:
+        if not '+' in expr_list:
+            break
+        else:
+            new_expr = []
+            number = None
+            summing = False
+
+            for component in expr_list:
+                if component == '*':
+                    new_expr.append(str(number))
+                    new_expr.append(component)
+                elif component == '+':
+                    summing = True
+                elif summing == False:
+                    number = int(component)
+                elif summing:
+                    number += int(component)
+                    summing = False
+
+            new_expr.append(str(number))
+
+        expr_list = new_expr
+
+    result = 1
+    operator = None
+
+    for component in expr_list:
+        if component == '*':
+            operator = component
+        elif operator is None:
+            result = int(component)
+        elif operator == '*':
+            result *= int(component)
+            operator = None
+
+    return result
 
 def find_parentheses(expr):
     start_idx = 0
@@ -32,15 +73,28 @@ def find_parentheses(expr):
 
     return start_idx, end_idx
 
-def collapse_parentheses(expr):
+def collapse_parentheses_basic(expr):
     while True:
         if '(' in expr:
             start_idx, end_idx = find_parentheses(expr)
-            expr = expr[:start_idx] + str(evaluate(expr[start_idx+1:end_idx])) + expr[end_idx+1:]
+            expr = expr[:start_idx] + str(evaluate_basic(expr[start_idx+1:end_idx])) + expr[end_idx+1:]
         else:
             break
 
-    number = evaluate(expr)
+    number = evaluate_basic(expr)
+
+    return number
+
+
+def collapse_parentheses_advanced(expr):
+    while True:
+        if '(' in expr:
+            start_idx, end_idx = find_parentheses(expr)
+            expr = expr[:start_idx] + str(evaluate_advanced(expr[start_idx+1:end_idx])) + expr[end_idx+1:]
+        else:
+            break
+
+    number = evaluate_advanced(expr)
 
     return number
 
@@ -50,14 +104,23 @@ curr_dir = os.path.dirname(os.path.abspath(__file__))
 f = open(os.path.join(curr_dir, "data.txt"))
 txt = f.readlines()
 f.close()
-
 txt_cleaned = [item.strip() for item in txt]
 
+# Part 1.
 total = 0
 
 for item in txt_cleaned:
-    print(item)
-    num = collapse_parentheses(item)
+    num = collapse_parentheses_basic(item)
     total += num
 
 print(f"Part 1 sum: {total}")
+
+
+# Part 2.
+total = 0
+
+for item in txt_cleaned:
+    num = collapse_parentheses_advanced(item)
+    total += num
+
+print(f"Part 2 sum: {total}")
