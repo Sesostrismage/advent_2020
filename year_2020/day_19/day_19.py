a8cda57a72726317ba99a25f78d3452a7cf530ef
Nick Weppenaar <weppenaar@gmail.com>
2020-12-20 07:23:15 +0100

    Now uses itertools to generate combinations regardsless of the number of nodes.

--- year_2020/day_19/day_19.py
+++ year_2020/day_19/day_19.py
@@ -1,34 +1,45 @@
+import itertools
 import os
 
 curr_dir = os.path.dirname(os.path.abspath(__file__))
 
 # f = open(os.path.join(curr_dir, "data.txt"))
-f = open(os.path.join(curr_dir, "test_1.txt"))
+# f = open(os.path.join(curr_dir, "test_1.txt"))
+f = open(os.path.join(curr_dir, "test_2.txt"))
 txt = f.readlines()
 f.close()
 
 txt_cleaned = [item.strip() for item in txt]
-print(txt_cleaned)
+# print(txt_cleaned)
 
 rule_dict = {}
+state = 'rules'
+check_list = []
 
 for line in txt_cleaned:
-    rule_num = int(line.split(':')[0])
-    rule_str = line.split(':')[1]
-    if 'a' in rule_str or 'b' in rule_str:
-        val = rule_str.strip(' "')
-    elif '|' in rule_str:
-        val = [
-            [int(item) for item in rule_str.split('|')[0].split()],
-            [int(item) for item in rule_str.split('|')[1].split()]
-        ]
-    else:
-        val = [int(item) for item in rule_str.split()]
+    if line == '':
+        state = 'checks'
+
+    elif state == 'rules':
+        rule_num = int(line.split(':')[0])
+        rule_str = line.split(':')[1]
+        if 'a' in rule_str or 'b' in rule_str:
+            val = rule_str.strip(' "')
+        elif '|' in rule_str:
+            val = [
+                [int(item) for item in rule_str.split('|')[0].split()],
+                [int(item) for item in rule_str.split('|')[1].split()]
+            ]
+        else:
+            val = [int(item) for item in rule_str.split()]
 
 
-    rule_dict[rule_num] = val
+        rule_dict[rule_num] = val
 
-print(rule_dict)
+    elif state == 'checks':
+        check_list.append(line)
+
+# print(rule_dict)
 
 class RuleRoot:
     def __init__(self, rd):
@@ -37,6 +48,17 @@ class RuleRoot:
 
         for r in self.rd[0]:
             self.base_rule.append(RuleNode(self.rd, r))
+
+    def yield_str(self):
+        temp_list = []
+        for node in self.base_rule:
+            temp_list.append(node.yield_str())
+
+        iter_list = list(itertools.product(*temp_list))
+        out_list = ["".join(item) for item in iter_list]
+
+        return out_list
+
 
 class RuleNode:
     def __init__(self, rd, r_key):
@@ -47,8 +69,8 @@ class RuleNode:
         rule_val = self.rd[r_key]
 
         if isinstance(rule_val, str):
-            self.node_rule = r_key
-            self.node_type = 'end'
+            self.node_rule = rule_val
+            self.node_type = 'str'
         else:
             if isinstance(rule_val[0], int):
                 self.node_rule = []
@@ -65,4 +87,38 @@ class RuleNode:
                     for s in rule_val[n]:
                         self.node_rule[n].append(RuleNode(self.rd, s))
 
+    def yield_str(self):
+        if self.node_type == 'str':
+            out_list = [self.node_rule]
+        elif self.node_type == 'linear':
+            temp_list = []
+            for node in self.node_rule:
+                temp_list.append(node.yield_str())
+
+            iter_list = list(itertools.product(*temp_list))
+            out_list = ["".join(item) for item in iter_list]
+        elif self.node_type == 'split':
+            out_list = []
+
+            for i in self.node_rule:
+                temp_list = []
+                for node in self.node_rule[i]:
+                    temp_list.append(node.yield_str())
+
+                iter_list = list(itertools.product(*temp_list))
+                out_list += ["".join(item) for item in iter_list]
+
+        return out_list
+
 rr = RuleRoot(rule_dict)
+# print(rr.base_rule)
+str_list = rr.yield_str()
+print(str_list)
+
+match_count = 0
+
+for line in check_list:
+    if line in str_list:
+        match_count += 1
+
+print(match_count)
