52d8c375690347deb1e4201c7c3957fcfe3cc2b5
Nick Weppenaar <weppenaar@gmail.com>
2020-12-21 07:09:55 +0100

    Day 21 part 1 solved.

--- year_2020/day_21/day_21.py
+++ year_2020/day_21/day_21.py
@@ -2,14 +2,23 @@ import os
 
 curr_dir = os.path.dirname(os.path.abspath(__file__))
 
-# f = open(os.path.join(curr_dir, "data.txt"))
-f = open(os.path.join(curr_dir, "test.txt"))
+f = open(os.path.join(curr_dir, "data.txt"))
+# f = open(os.path.join(curr_dir, "test.txt"))
 # f = open(os.path.join(curr_dir, "test_2.txt"))
 txt = f.readlines()
 f.close()
 
 txt_cleaned = [item.strip() for item in txt]
-print(txt_cleaned)
+# print(txt_cleaned)
+
+def get_ingredients(l):
+    out_list = []
+
+    for item in l:
+        out_list.append(item.split(' (contains ')[0].split())
+
+    return out_list
+
 
 def get_possible_allergens_dict(l):
     fad = {}
@@ -47,13 +56,31 @@ def get_possible_allergens_dict(l):
                 pad[key] = pad[key][0]
                 remove_list.append(pad[key])
                 change = True
+                break
             elif isinstance(pad[key], list):
                 for remove_item in remove_list:
                     if remove_item in pad[key]:
                         pad[key].remove(remove_item)
 
+    al = []
 
-    return pad
+    for key in pad:
+        if isinstance(pad[key], str):
+            al.append(pad[key])
+        else:
+            al += pad[key]
 
-allergen_dict = get_possible_allergens_dict(txt_cleaned)
+    return pad, al
+
+ingredient_list = get_ingredients(txt_cleaned)
+# print(ingredient_list)
+allergen_dict, allergen_list = get_possible_allergens_dict(txt_cleaned)
 print(allergen_dict)
+# print(allergen_list)
+
+non_allergen_count = 0
+
+for ingredients in ingredient_list:
+    non_allergen_count += len([item for item in ingredients if item not in allergen_list])
+
+print(non_allergen_count)
