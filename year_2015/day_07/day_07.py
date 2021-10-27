576a14fb8785e2977218d069b2e701b14f3091d8
Nick Weppenaar <weppenaar@gmail.com>
2021-10-27 18:35:05 +0200

    Day 7 part 1.

--- year_2015/day_07/day_07.py
+++ year_2015/day_07/day_07.py
@@ -1,3 +1,4 @@
+import copy
 import logging
 import os
 import pandas as pd
@@ -7,35 +8,71 @@ logging.basicConfig(format="%(message)s", level=logging.DEBUG)
 
 # Part 1.
 f = open(os.path.join(curr_dir, "input_day_07.txt"), "r")
-input_day_07 = f.read().split("\n")
+instructions = f.read().split("\n")
 f.close()
 
-print(input_day_07)
-
 series = pd.Series()
+leftover_instructions = []
+count = 0
 
-for line in input_day_07:
-    split_line = line.split("->")
-    operation = split_line[0].strip()
-    receiver = split_line[1].strip()
+while True:
+    print(f"Iteration {count}, {len(instructions)} instructions remaining.")
+    for line in instructions:
+        split_line = line.split("->")
+        operation = split_line[0].strip()
+        receiver = split_line[1].strip()
 
-    if operation.isnumeric():
-        series.loc[receiver] = int(operation)
-    elif "NOT" in operation:
-        inp = series[operation.split()[-1]]
-        series.loc[receiver] = 65535 - inp
+        if len(operation.split()) == 1:
+            opstrip = operation.strip()
+
+            if opstrip.isnumeric():
+                series.loc[receiver] = int(opstrip)
+            elif opstrip in series.index:
+                series.loc[receiver] = series.loc[opstrip]
+            else:
+                leftover_instructions.append(line)
+
+        elif "NOT" in operation:
+            a = operation.split()[-1]
+
+            if a in series.index:
+                inp = series[a]
+                series.loc[receiver] = 65535 - inp
+            else:
+                leftover_instructions.append(line)
+        else:
+            a = operation.split()[0]
+            b = operation.split()[-1]
+
+            if "AND" in operation:
+                if a.isnumeric() and b in series.index:
+                    series.loc[receiver] = int(a) & series[b]
+                elif a in series.index and b in series.index:
+                    series.loc[receiver] = series[a] & series[b]
+                else:
+                    leftover_instructions.append(line)
+            elif "OR" in operation:
+                if a in series.index and b in series.index:
+                    series.loc[receiver] = series[a] | series[b]
+                else:
+                    leftover_instructions.append(line)
+            elif "LSHIFT" in operation:
+                if a in series.index:
+                    series.loc[receiver] = series[a] << int(b)
+                else:
+                    leftover_instructions.append(line)
+            elif "RSHIFT" in operation:
+                if a in series.index:
+                    series.loc[receiver] = series[a] >> int(b)
+                else:
+                    leftover_instructions.append(line)
+
+    if len(leftover_instructions) == 0:
+        break
     else:
-        a = operation.split()[0]
-        b = operation.split()[-1]
-
-        if "AND" in operation:
-            series.loc[receiver] = series[a] & series[b]
-        elif "OR" in operation:
-            series.loc[receiver] = series[a] | series[b]
-        elif "LSHIFT" in operation:
-            series.loc[receiver] = series[a] << int(b)
-        elif "RSHIFT" in operation:
-            series.loc[receiver] = series[a] >> int(b)
+        instructions = copy.deepcopy(leftover_instructions)
+        leftover_instructions = []
+        series.sort_index(inplace=True)
 
 
-print(series.sort_index())
+print(series.loc["a"])
