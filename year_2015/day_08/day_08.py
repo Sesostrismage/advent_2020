68243e2fafa228fbf3fcc4653e01206bf032eb2c
Nick Weppenaar <weppenaar@gmail.com>
2021-10-28 20:50:46 +0200

    Day 8.

--- year_2015/day_08/day_08.py
+++ year_2015/day_08/day_08.py
@@ -3,11 +3,11 @@ import re
 
 curr_dir = os.path.dirname(os.path.abspath(__file__))
 
-# Part 1.
 f = open(os.path.join(curr_dir, "input_day_08.txt"), "r")
 puzzle_input = f.read().split("\n")
 f.close()
 
+# Part 1.
 total_code_length = 0
 total_str_length = 0
 
@@ -24,3 +24,22 @@ for line in puzzle_input:
     total_str_length += str_length
 
 print(f"Part 1: {total_code_length-total_str_length}")
+
+
+# Part 2.
+total_code_length = 0
+total_encoded_length = 0
+
+for line in puzzle_input:
+    code_length = len(line)
+    encoded_length = len(line) + 2
+    encoded_length += line.count('"')
+    encoded_length += line.count("\\")
+    # print(f"Code length: {code_length}")
+    # print(f"Encoded length: {encoded_length}")
+
+    total_code_length += code_length
+    total_encoded_length += encoded_length
+
+
+print(f"Part 2: {total_encoded_length-total_code_length}")
