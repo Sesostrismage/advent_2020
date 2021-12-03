8ac62ac7959110629d8c864fab18cd3c691dfc1f
Nick Weppenaar <weppenaar@gmail.com>
2021-12-03 11:13:52 +0100

    Day 3.

--- year_2021/day_03/day_03.py
+++ year_2021/day_03/day_03.py
@@ -8,20 +8,56 @@ with open(input_path, "r") as f:
     puzzle_input = f.readlines()
 
 puzzle_input = [[sub_item for sub_item in item.strip()] for item in puzzle_input]
-df = pd.DataFrame.from_records(puzzle_input)
-print(df)
-print(df[0].value_counts())
+puzzle_df = pd.DataFrame.from_records(puzzle_input)
 
+# Part 1.
 gamma_rate = ""
 epsilon_rate = ""
 
-for col in df.columns:
-    vc = df[col].value_counts()
-    gamma_bit = vc.idxmax()
-    gamma_rate += gamma_bit
-    epsilon_bit = vc.idxmin()
-    epsilon_rate += epsilon_bit
+for col in puzzle_df.columns:
+    vc = puzzle_df[col].value_counts()
+    gamma_rate += vc.idxmax()
+    epsilon_rate += vc.idxmin()
 
 gamma_rate = int(gamma_rate, 2)
 epsilon_rate = int(epsilon_rate, 2)
-print(gamma_rate * epsilon_rate)
+print(f"Part 1: {gamma_rate * epsilon_rate}")
+
+
+# Part 2.
+def bit_criteria(df: pd.DataFrame, system: str) -> int:
+    col_list = df.columns
+    rating_df = df.copy()
+
+    for col in col_list:
+        vc = rating_df[col].value_counts()
+
+        if len(vc) == 1:
+            val = vc.index[0]
+        elif system == "og":
+            if vc.loc["1"] >= vc.loc["0"]:
+                val = "1"
+            else:
+                val = "0"
+        elif system == "co2":
+            if vc.loc["0"] <= vc.loc["1"]:
+                val = "0"
+            else:
+                val = "1"
+
+        rating_df = rating_df[rating_df[col] == val]
+
+        if len(rating_df) == 1:
+            rating_str = "".join([rating_df.iloc[0, idx] for idx in rating_df.columns])
+            rating = int(rating_str, 2)
+            break
+
+    return rating
+
+
+# Oxygen generator rating.
+og_rating = bit_criteria(puzzle_df, "og")
+# CO2 scrubber rating.
+co2_rating = bit_criteria(puzzle_df, "co2")
+
+print(f"Part 2: {og_rating * co2_rating}")
