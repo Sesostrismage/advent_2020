3436fc4c04a1cf845c645f1d2abe498f25d2c72f
Nick Weppenaar <weppenaar@gmail.com>
2021-12-17 08:00:28 +0100

    Day 17 part 2.

--- year_2021/day_17/day_17.py
+++ year_2021/day_17/day_17.py
@@ -8,7 +8,6 @@ input_path = os.path.join(curr_dir, "puzzle_input_day_17.txt")
 with open(input_path, "r") as f:
     puzzle_input = f.readlines()[0]
 
-print(puzzle_input)
 x_part = puzzle_input.split(':')[1].split(',')[0].split('=')[1]
 x_min = int(x_part.split('..')[0])
 x_max = int(x_part.split('..')[1])
@@ -16,32 +15,28 @@ y_part = puzzle_input.split(':')[1].split(',')[1].split('=')[1]
 y_min = int(y_part.split('..')[0])
 y_max = int(y_part.split('..')[1])
 
-print(x_min, x_max, y_min, y_max)
+hit_set = set()
+highest_height = 0
 
-highest_y_velocity_hit = 0
+for x_velocity_init in range(1,x_max+1):
+    for y_velocity_init in range(y_min, 4*abs(y_min)):
+        x_pos = 0
+        x_velocity = x_velocity_init
+        y_pos = 0
+        y_velocity = y_velocity_init
+        max_height = y_pos
 
-for y_velocity_init in range(1, 4*abs(y_min)):
-    y_step = 0
-    y_velocity = y_velocity_init
-    timesteps = 0
+        while (y_pos >= y_min) and (x_pos <= x_max):
+            x_pos += x_velocity
+            y_pos += y_velocity
+            max_height = max(max_height, y_pos)
 
-    while y_step >= y_min:
-        y_step += y_velocity
-        y_velocity -= 1
-        timesteps += 1
+            if (y_min <= y_pos <= y_max) and (x_min <= x_pos <= x_max):
+                highest_height = max(highest_height, max_height)
+                hit_set.add((x_velocity_init, y_velocity_init))
 
-        if y_min <= y_step <= y_max:
-            print(f"Y hit! Position: {y_step}, initial velocity: {y_velocity_init}")
+            x_velocity = max(0, x_velocity - 1)
+            y_velocity -= 1
 
-            x_velocity = 1
-
-            while True:
-                x_pos = x_velocity * (x_velocity - 1)
-                if x_pos < x_min:
-                    x_velocity += 1
-                elif x_min <= x_pos <= x_max:
-                    print(f"Complete hit! Position: {x_pos}, {y_step}, initial velocity: {y_velocity_init}")
-                    print(f"Greatest height: {y_velocity_init * (y_velocity_init +1)/2}")
-                    break
-                elif x_pos > x_max:
-                    break
+print(f"Part 1: {int(highest_height)}")
+print(f"Part 2: {len(hit_set)}")
